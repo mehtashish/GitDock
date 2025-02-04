23627371df250b147542779fb6548088c43332de
Akarsh Rai <akarshrai1405@gmail.com>
2025-02-04 14:13:55 +0530

    Sidebar issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,13 @@ import HomePage from "./pages/HomePage";
 import ExplorePage from "./pages/ExplorePage";
 import LikesPage from "./pages/LikesPage";
 
-function App() {
+import Sidebar from "./components/Sidebar";
+
+function App() { 
   return (
-    <div className="flex text-white">
-      <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+    <div className='flex text-white'>
+      <Sidebar />
+      <div className='max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1'>
         <Routes>
         <Route path='/' element={<HomePage />} />
 					<Route path='/login' element={<LoginPage />} />
